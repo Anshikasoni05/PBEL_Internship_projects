b382b2b762fe9201ff1c6ee72d26f0165d612b65
Anshika soni <sonianshi8881@gmail.com>
2026-07-19 19:50:47 +0530

    Added Flask backend server

---
+++ CampusAI/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+
+from src.predictions.chat_engine import get_response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    user_message = request.json.get("message", "")
+
+    response = get_response(user_message)
+
+    return jsonify({
+        "response": response
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
